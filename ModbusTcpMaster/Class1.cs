ba898ef1178d77e2111579220229f562fe360301
I2h4n <I2h4n@TOMMY>
2018-09-17 21:37:19 +0800

    添加项目文件。

---
+++ ModbusTcpMaster/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusTcpMaster
+{
+    public class Class1
+    {
+    }
+}
